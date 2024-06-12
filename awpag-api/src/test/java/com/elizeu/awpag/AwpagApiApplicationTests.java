8a44e909e8bafb70371c9abefbf97385e4a834a7
elizeuOliJunior <elizeugojr@gmail.com>
2024-06-12 14:13:28 -0300

    initializing spring

---
+++ awpag-api/src/test/java/com/elizeu/awpag/AwpagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elizeu.awpag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwpagApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
