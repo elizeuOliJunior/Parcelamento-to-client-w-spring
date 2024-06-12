7ec925875ab0a295f6e7481a28212c1863a5e6b0
elizeuOliJunior <elizeugojr@gmail.com>
2024-06-12 15:32:20 -0300

    creating client entity class

--- awpag-api/src/main/java/com/elizeu/awpag/api/controller/ClienteController.java
+++ awpag-api/src/main/java/com/elizeu/awpag/api/controller/ClienteController.java
@@ -1,13 +1,31 @@
 package com.elizeu.awpag.api.controller;
 
+import com.elizeu.awpag.domain.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class ClienteController {
 
     @GetMapping("/clientes")
-    public String listar(){
-        return "Teste";
+    public List<Cliente> listar(){
+        var cliente1 = new Cliente();
+        cliente1.setId(1l);
+        cliente1.setNome("Elizeu");
+        cliente1.setEmail("elizeu@gmail.com");
+        cliente1.setTelefone("111-222-333");
+
+        var cliente2 = new Cliente();
+        cliente2.setId(2l);
+        cliente2.setNome("Heloisa");
+        cliente2.setEmail("heloisa@gmail.com");
+        cliente2.setTelefone("222-333-444");
+
+        return Arrays.asList(cliente1,cliente2);
     }
 }
