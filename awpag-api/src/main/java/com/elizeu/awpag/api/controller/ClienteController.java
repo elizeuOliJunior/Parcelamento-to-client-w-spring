a42a0388034301010611c2b05a01be1e642abb43
elizeuOliJunior <elizeugojr@gmail.com>
2024-06-13 16:00:53 -0300

    Implementation of interface with jakarta

--- awpag-api/src/main/java/com/elizeu/awpag/api/controller/ClienteController.java
+++ awpag-api/src/main/java/com/elizeu/awpag/api/controller/ClienteController.java
@@ -1,8 +1,11 @@
 package com.elizeu.awpag.api.controller;
 
 import com.elizeu.awpag.domain.model.Cliente;
+import com.elizeu.awpag.domain.repository.ClienteRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +14,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    private final ClienteRepository clienteRepository;
 
     @GetMapping("/clientes")
     public List<Cliente> listar(){
-        return manager.createQuery("from Cliente",Cliente.class).
-                getResultList();
+
+        //return clienteRepository.findByNomeContainig("JO");
+        //return clienteRepository.findByNome("JOAO");
+        return clienteRepository.findAll();
     }
 }
---
+++ awpag-api/src/main/java/com/elizeu/awpag/domain/repository/ClienteRepository.java
@@ -0,0 +1,15 @@
+package com.elizeu.awpag.domain.repository;
+
+import com.elizeu.awpag.domain.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ClienteRepository extends JpaRepository <Cliente,Long>{
+
+    List<Cliente> findByNome(String nome);
+
+    List<Cliente> findByNomeContainig(String nome);
+}
